bf0e89390bfc50057b13b8e1f9fb01efbf5d92e0
Csaba Steiner <stecsabi@gmail.com>
2020-02-19 08:58:11 +0100

    First commit of the bot.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twitch_chat_bot
 Twitch chat bot written in javascript (node.js). Some cfg file may refer to specific streamer.
+
+
+
---
+++ app.js
@@ -0,0 +1,222 @@
+const fs = require("fs");
+const tmi = require("tmi.js");
+const logger = require("tmi.js/lib/logger");
+
+const config = JSON.parse(fs.readFileSync("app.cfg.json"));
+const moderators = JSON.parse(fs.readFileSync("moderators.json"))
+let commands = JSON.parse(fs.readFileSync(config.commands));
+let viewers = [];
+
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+let client = new tmi.Client({
+    identity: {
+        username: config.username,
+        password: config.password
+    },
+    channels: [config.channel],
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    }
+});
+
+client.connect();
+
+process.on("SIGINT", () => {
+    client.disconnect()
+        .then(() => {
+            fs.writeFileSync(
+                config.commands,
+                JSON.stringify(commands.sort((a, b) => {
+                    if (a.name < b.name) { return -1; }
+                    else if (a.name > b.name) { return 1; }
+                    else { return 0; }
+                }), null, 4) + "\n",
+                () => {
+                    logger.warn(`Could not save commands to ${config.commands}!`);
+                }
+            );
+
+            process.exit();
+        });
+});
+
+client.on("chat", (channel, userstate, commandMessage, self) => {
+    if (self) { return; };
+    if (!config.verbose) { return; };
+
+    /*if (!viewers.includes(userstate.username)) {
+        viewers.push(userstate.username);
+        client.say(channel, `Üdvözlünk a streamen, ${userstate["display-name"]} HeyGuys`);
+    }*/
+
+    if (!commandMessage.startsWith("!")) { return; };
+	//if (userstate.username != "unnamedman22") {return; }; //Only answers to me
+	
+    let commandName = commandMessage.split(/\s/)[0].toLowerCase();
+    commandMessage = commandMessage.slice(commandName.length).trim();
+
+    switch (commandName) {
+        case ("!commands"):
+            (() => {
+                let names = "!commands !add !remove !uptime"
+                    .split(/\s/)
+                    .concat(
+                        commands.map(c => {
+                            if (c.active) { return c.name; }
+                            else { return null; }
+                        }).filter(name => {
+                            return name !== null
+                        })
+                    ).sort()
+                    .join(", ");
+
+                client.say(channel, `@${userstate.username} A következő parancsok elérhetőek: ${names}`);
+            })();
+            break;
+        case ("!add"):
+            (() => {
+				if (!modcheck(userstate.username))
+					return;
+                const usage = `@${userstate.username} Használat: !add "parancs" "üzenet"`;
+
+                if (!commandMessage) { client.say(channel, usage); return; }
+
+                commandName = commandMessage.split(/\s/)[0];
+                if (!commandName) { client.say(channel, usage); return; }
+
+                commandMessage = commandMessage.substr(commandName.length);
+                if (!commandMessage) { client.say(channel, usage); return; }
+
+                commandName = (commandName.startsWith("!") ? "" : "!") + commandName;
+                commandName = commandName.toLowerCase();
+
+                let commandNames = "!commands !add !remove"
+                    .split(/\s/)
+                    .concat(
+                        commands.map(command => {
+                            if (!command.active) { return null; }
+                            return command.name;
+                        }).filter(name => {
+                            return name !== null
+                        })
+                    );
+
+                if (commandNames.includes(commandName)) {
+                    client.say(channel, `@${userstate.username} A parancs "${commandName}" már létezik!`); return;
+                }
+
+                commands.push({
+                    name: commandName,
+                    message: commandMessage,
+                    author: userstate.username,
+                    active: true
+                });
+
+                client.say(channel, `@${userstate.username} Parancs "${commandName}" hozzáadva!`);
+            })();
+            break;
+        case ("!remove"):
+            (() => {
+				if (!modcheck(userstate.username))
+					return;
+                commandMessage = commandMessage.split(/\s/)[0];
+                if (!commandMessage) { client.say(channel, `@${userstate.username} Használat: !remove "parancs"!`); return; }
+
+                commandMessage = (commandMessage.startsWith("!") ? "" : "!") + commandMessage.toLowerCase();
+
+                if ("!commands !add !remove".split(/\s/).includes(commandMessage)) {
+                    client.say(channel, `@${userstate.username} A parancs "${commandMessage}" nem törölhető!`);
+                    return;
+                }
+
+                let command = commands
+                    .filter(c => {
+                        return c.name === commandMessage && c.active;
+                    });
+
+                if (command.length <= 0) {
+                    // client.say(channel, `@${userstate.username} The command ${commandMessage} does not exist!`);
+                    return;
+                }
+
+                command = command[0];
+
+                /*if (command.author !== userstate.username) {
+                    client.say(channel, `@${userstate.username} A parancs "${commandMessage}" nem törölhető, mert nem te vagy a tulajdonosa!`);
+                    return;
+                }*/
+
+                commands = commands
+                    .filter(c => {
+                        return c.name !== command.name;
+                    });
+
+                client.say(channel, `@${userstate.username} A parancs "${commandMessage}" törölve!`);
+            })();
+            break;
+		case ("!uptime"):
+			const request = require('request');
+			request('http://decapi.me/twitch/uptime?channel=bavaz1', { json: true }, (err, body) => {
+				if (err) { return console.log(err); }
+				console.log(body.body);
+				var uptime = body.body
+				if(uptime == "bavaz1 is offline"){
+					uptime = "bavaz1 közvetítése jelenleg offline!"
+				}
+				else {
+					var tmp1 = uptime.replace("minutes,", "perc");
+					var tmp2 = tmp1.replace("seconds", "másodperc");
+					uptime = tmp2;
+				}
+				client.say(channel, uptime);
+			
+			});
+		/*case ("!teszt2"):
+			console.log(moderators[1].name);
+			console.log(moderators[0].name);
+			console.log(moderators.length);*/
+        default:
+            (() => {
+                let command = commands
+                    .filter(c => {
+                        return c.name === commandName && c.active;
+                    });
+
+                if (command.length <= 0) { return; }
+                command = command[0];
+
+                if (`#${command.author}` === channel) {
+                    client.say(channel, `${command.message}`);
+                }
+                else {
+                    client.say(channel, `${command.message}`);
+                }
+            })();
+            break;
+    }
+});
+
+function modcheck(username){
+	var i = 0
+	while (i < moderators.length) //Looping through modlist
+		{
+			if (moderators[i].name == username)
+				return true;
+			i++;
+		}
+	var errormessage = "Permission error for " + username + "!";
+	console.log(errormessage)
+	return false;
+}
